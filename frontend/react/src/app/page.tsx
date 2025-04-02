181e0d7a90ac326f849db83169e381ff117e600b
Syknapse <29199184+Syknapse@users.noreply.github.com>
2025-04-02 10:31:22 +0200

    Create HomeGrid component

--- frontend/react/src/app/page.tsx
+++ frontend/react/src/app/page.tsx
@@ -5,10 +5,12 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl sm:text-5xl font-bold text-center">
-          Welcome to ScalaDays ID Validator
-        </h1>
-        <HomeGrid />
+        <div className="flex gap-14 items-center flex-col">
+          <h1 className="text-4xl sm:text-5xl font-bold text-center">
+            Welcome to ScalaDays ID Validator
+          </h1>
+          <HomeGrid />
+        </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
--- frontend/react/src/components/features/HomeGrid/HomeGrid.tsx
+++ frontend/react/src/components/features/HomeGrid/HomeGrid.tsx
@@ -1,10 +1,21 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import data from "./data";
 
 const HomeGrid: React.FC = () => {
   return (
-    <div className="flex gap-4 items-center flex-col sm:flex-row">
-      <Button onClick={() => console.log("Clicked!!")}>Click me</Button>
+    <div className="grid grid-cols-1 [@media(min-width:450px)]:grid-cols-3 gap-8 sm:min-w-[500px]">
+      {data.map((item) => (
+        <Button
+          key={item.href}
+          asChild
+          variant="outline"
+          className="w-full h-12 cursor-pointer sm:w-auto"
+        >
+          <Link href={"/raw-class"}>{item.title}</Link>
+        </Button>
+      ))}
     </div>
   );
 };
