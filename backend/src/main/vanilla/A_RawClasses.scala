3fea85f278ed2aaaa5ffb17b0f31324e49d2f38c
David <david.a.gil@proton.me>
2025-04-25 16:00:27 +0200

    Encapsulation and Refactor of RawClasses

--- backend/src/main/vanilla/A_RawClasses.scala
+++ backend/src/main/vanilla/A_RawClasses.scala
@@ -1,7 +1,5 @@
 package backend.vanilla
 
-import backend.common.*
-
 /** =Regular Classes in Scala=
   *
   * A regular class is defined using the 'class' keyword.
@@ -33,62 +31,85 @@ import backend.common.*
 
 object A_RawClasses:
 
+  // Do NOT change the order of the enumeration.
+  // The ordinal value of each letter corresponds with number they represent
+  enum NieLetter:
+    case X // 0
+    case Y // 1
+    case Z // 2
+
+  // Do NOT change the order of the enumeration.
+  // The ordinal value of each letter corresponds with the remainder of number divided by 23  
+  enum ControlLetter:
+    case T // 0
+    case R // 1
+    case W // 2
+    case A // 3
+    case G // 4
+    case M // 5
+    case Y // 6
+    case F // 7
+    case P // 8
+    case D // 9
+    case X // 10
+    case B // 11
+    case N // 12
+    case J // 13
+    case Z // 14
+    case S // 15
+    case Q // 16
+    case V // 17
+    case H // 18
+    case L // 19
+    case C // 20
+    case K // 21
+    case E // 22  
+
   sealed trait ID
 
-  private[vanilla] final class DNI(number: String, letter: String) extends ID:
-    require(number.forall(_.isDigit), s"number $number should not contain letters")
-    require(number.length == 8, s"number $number should contain 8 digits")
-    val _number: Int = number.toInt
-    require(_number >= 0, s"'$number' is negative. It must be positive")
-    require(_number <= 99999999, s"'$number' is too big. Max number is 99999999")
+  private[vanilla] final class DNI private(number: String, letter: ControlLetter) extends ID:
+    require(number.forall(_.isDigit), s"DNI number '$number' should not contain letters")
+    require(number.length == 8, s"DNI number '$number' should contain 8 digits")
+    val _number = number.toInt
     require(
-      ControlLetter.values.map(_.toString).contains(letter),
-      s"'$letter' is not a valid ID letter"
+      ControlLetter.fromOrdinal(_number % 23) == letter,
+      s"DNI number '$number' does not match the control letter '$letter'"
     )
-    require(
-      ControlLetter.isValidId(_number, ControlLetter.valueOf(letter)),
-      "Number does not match correct control letter"
-    )
-
     override def toString: String = s"$number-$letter"
 
-  private[vanilla] final class NIE(nieLetter: String, number: String, letter: String) extends ID:
-    require(number.forall(_.isDigit), s"number $number should not contain letters")
-    require(number.length == 7, s"number $number should contain 7 digits")
-    val _number: Int = number.toInt
+  private[vanilla] object DNI:
+    def apply(number: String, letter: String): DNI =
+      val _letter = ControlLetter.valueOf(letter)
+      new DNI(number, _letter)
+
+  private[vanilla] final class NIE private(nieLetter: NieLetter, number: String, letter: ControlLetter) extends ID:
+    require(number.forall(_.isDigit), s"NIE number '$number' should not contain letters")
+    require(number.length == 7, s"NIE number '$number' should contain 7 digits")
+    val ordinalOfNIE = nieLetter.ordinal // Extracts the number representation of the NIE Letter
+    val _number = s"$ordinalOfNIE$number".toInt // Appends the number representation from NIE Letter to the number
     require(
-      NieLetter.values.map(_.toString).contains(nieLetter),
-      s"'$nieLetter' is not a valid NIE letter"
-    )
-    require(_number >= 0, s"'$number' is negative. It must be positive")
-    require(_number <= 99999999, s"'$number' is too big. Max number is 99999999")
-    require(
-      ControlLetter.values.map(_.toString).contains(letter),
-      s"'$letter' is not a valid ID letter"
-    )
-    require(
-      ControlLetter.isValidId(s"${NieLetter.valueOf(nieLetter).ordinal}$number".toInt, ControlLetter.valueOf(letter)),
-      "Number does not match correct control letter"
+      ControlLetter.fromOrdinal(_number % 23) == letter,
+      s"NIE number '$number' does not match the control letter '$letter'"
     )
     override def toString: String = s"$nieLetter-$number-$letter"
 
+  private [vanilla] object NIE:
+    def apply(nieLetter: String, number: String, letter: String): NIE =
+      val _nieLetter = NieLetter.valueOf(nieLetter)
+      val _letter = ControlLetter.valueOf(letter)
+      new NIE(_nieLetter, number, _letter)  
+
   object ID:
     def apply(input: String): ID =
-      val trimmed = input.trim
-      val withoutDash = trimmed.replace("-", "")
-      if withoutDash.head.isDigit
+      val trimmed = input.trim // Handeling empty spaces around
+      val withoutDash = trimmed.replace("-", "") // Removing dashes
+      if withoutDash.head.isDigit // Splitting between DNI and NIE
       then
         val (number, letter) = withoutDash.splitAt(withoutDash.length - 1)
-
-        DNI(
-          number = number,
-          letter = letter.toUpperCase()
-        )
+        val _letter = letter.toUpperCase // Handeling lowerCase
+        DNI(number, _letter) 
       else
         val (number, letter) = withoutDash.tail.splitAt(withoutDash.length - 2)
-
-        NIE(
-          nieLetter = withoutDash.head.toString.toUpperCase(),
-          number = number,
-          letter = letter.toUpperCase()
-        )
+        val nieLetter = withoutDash.head.toString.toUpperCase // Handling lowerCase
+        val _letter = letter.toUpperCase // Handling lowerCase
+        NIE(nieLetter, number, _letter)
