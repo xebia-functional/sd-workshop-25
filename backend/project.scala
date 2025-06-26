d8d6f9995061c62c2dc27fc684e22be4c4ba0221
David <david.a.gil@proton.me>
2025-06-26 15:20:15 +0200

    update dependencies

--- backend/project.scala
+++ backend/project.scala
@@ -1,4 +1,4 @@
 //> using dependency com.lihaoyi::cask:0.10.2
-//> using dependencies com.lihaoyi::upickle:4.1.0
-//> using dependency io.github.kitlangton::neotype:0.3.24
+//> using dependencies com.lihaoyi::upickle:4.2.1
+//> using dependency io.github.kitlangton::neotype:0.3.25
 //> using test.dep com.lihaoyi::utest:0.8.5
