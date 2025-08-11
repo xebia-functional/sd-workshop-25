ee64b275685e8bf0640fd37ee97120860b1f7c80
David <david.a.gil@proton.me>
2025-08-11 02:48:51 +0200

    Validated that the program runs and reports the missing implementation

--- sandbox/Errors.scala
+++ sandbox/Errors.scala
@@ -9,29 +9,14 @@ object Errors:
    * Come up with tailored error messages you want to give back to the user in the UI
    */
   object messages:
-    //def invalidInput(input: String): String = ???
-    //def invalidNumber(number: String): String = ???
-    //def invalidDniNumber(dniNumber: String): String = ???
-    //def invalidNieNumber(nieNumber: String): String = ???
-    //def invalidNieLetter(nieLetter: String): String = ???
-    //def invalidControlLetter(controlLetter: String): String = ???
-    //def invalidDni(dniNumber: String, letter: ControlLetter): String = ???
-    //def invalidNie(nieLetter: NieLetter, nieNumber: String, letter: ControlLetter): String = ???
-    def invalidInput(input: String): String = s"Invalid Input: '$input' should be AlphaNumeric and have 9 characters"
-
-    def invalidNumber(number: String): String = s"Invalid Number: '$number' should not contain letters"
-
-    def invalidDniNumber(dniNumber: String): String = s"Invalid DNI Number: '$dniNumber' should contain 8 digits"
-
-    def invalidNieNumber(nieNumber: String): String = s"Invalid NIE Number: '$nieNumber' should contain 7 digits"
-
-    def invalidNieLetter(nieLetter: String): String = s"Invalid NIE Letter: '$nieLetter' is not a valid NIE letter"
-
-    def invalidControlLetter(controlLetter: String): String = s"Invalid ControlLetter: '$controlLetter' is not a valid Control letter"
-
-    def invalidDni(dniNumber: String, letter: ControlLetter): String = s"Invalid DNI: '$dniNumber' does not match the control letter '$letter'"
-
-    def invalidNie(nieLetter: NieLetter, nieNumber: String, letter: ControlLetter): String = s"Invalid NIE: '$nieLetter-$nieNumber' does not match the control letter '$letter'"
+    def invalidInput(input: String): String = ???
+    def invalidNumber(number: String): String = ???
+    def invalidDniNumber(dniNumber: String): String = ???
+    def invalidNieNumber(nieNumber: String): String = ???
+    def invalidNieLetter(nieLetter: String): String = ???
+    def invalidControlLetter(controlLetter: String): String = ???
+    def invalidDni(dniNumber: String, letter: ControlLetter): String = ???
+    def invalidNie(nieLetter: NieLetter, nieNumber: String, letter: ControlLetter): String = ???
 
   end messages
 
@@ -42,40 +27,15 @@ object Errors:
 
     import messages.*
 
-    //def requireValidInput(input: String): Unit = ???
-    //def requireValidNumber(number: String): Unit = ???
-    //def requireValidDniNumber(dniNumber: String): Unit = ???
-    //def requireValidNieNumber(nieNumber: String): Unit = ???
-    //def requireValidNieLetter(nieLetter: String): Unit = ???
-    //def requireValidControlLetter(controlLetter: String): Unit = ???
-    //def requireValidDni(dniNumber: String, letter: ControlLetter): Unit = ???
-    //def requireValidNie(nieLetter: NieLetter, nieNumber: String, letter: ControlLetter): Unit = ???
-    def requireValidInput(input: String): Unit =
-      require(input.length == 9 && input.forall(_.isLetterOrDigit), invalidInput(input))
+    def requireValidInput(input: String): Unit = ???
+    def requireValidNumber(number: String): Unit = ???
+    def requireValidDniNumber(dniNumber: String): Unit = ???
+    def requireValidNieNumber(nieNumber: String): Unit = ???
+    def requireValidNieLetter(nieLetter: String): Unit = ???
+    def requireValidControlLetter(controlLetter: String): Unit = ???
+    def requireValidDni(dniNumber: String, letter: ControlLetter): Unit = ???
+    def requireValidNie(nieLetter: NieLetter, nieNumber: String, letter: ControlLetter): Unit = ???
 
-    def requireValidNumber(number: String): Unit =
-      require(number.forall(_.isDigit), invalidNumber(number))
-
-    def requireValidDniNumber(dniNumber: String): Unit =
-      require(dniNumber.length == 8, invalidDniNumber(dniNumber))
-
-    def requireValidNieNumber(nieNumber: String): Unit =
-      require(nieNumber.length == 7, invalidNieNumber(nieNumber))
-
-    def requireValidNieLetter(nieLetter: String): Unit =
-      require(NieLetter.values.map(_.toString).contains(nieLetter), invalidNieLetter(nieLetter))
-
-    def requireValidControlLetter(controlLetter: String): Unit =
-      require(ControlLetter.values.map(_.toString).contains(controlLetter), invalidControlLetter(controlLetter))
-
-    def requireValidDni(dniNumber: String, letter: ControlLetter): Unit =
-      require(dniNumber.toInt % 23 == letter.ordinal, invalidDni(dniNumber, letter))
-
-    def requireValidNie(nieLetter: NieLetter, nieNumber: String, letter: ControlLetter): Unit =
-      require(
-        s"${nieLetter.ordinal}$nieNumber".toInt % 23 == letter.ordinal,
-        invalidNie(nieLetter, nieNumber, letter)
-      )
   end requirements
 
 end Errors
